fcf69ee18aec7b44130191d98a6ce323c859e84c
André Dorneles <andre@imperiosoft.com.br>
2025-04-18 14:33:56 -0300

    Validaçao de cep e tratamento de erros

--- internal/handlers/temperature_handler.go
+++ internal/handlers/temperature_handler.go
@@ -19,20 +19,50 @@ func New(weatherAPI *weatherapi.WeatherAPI) *TemperatureHandler {
 	}
 }
 
+func (t *TemperatureHandler) validateZipCode(zipCode string) (string, error) {
+	cleanZip := ""
+	for _, char := range zipCode {
+		if char >= '0' && char <= '9' {
+			cleanZip += string(char)
+		}
+	}
+	if len(cleanZip) != 8 {
+		return "", fmt.Errorf("invalid zipcode: must contain exactly 8 digits")
+	}
+	return cleanZip, nil
+}
+
 func (t *TemperatureHandler) GetTemperature(w http.ResponseWriter, r *http.Request) {
 	zipCode := chi.URLParam(r, "zipCode")
-	fmt.Println(zipCode)
 
-	city, err := viacep.GetCityByZipCode(zipCode)
+	cleanZip, err := t.validateZipCode(zipCode)
 	if err != nil {
-		panic(err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		w.Write([]byte("invalid zipcode"))
+		return
+	}
+
+	city, err := viacep.GetCityByZipCode(cleanZip)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+		return
+	}
+	if city == "" {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("can not find zipcode"))
 	}
 
 	weatherResponse, err := t.WeatherAPI.GetTempByCity(city)
 	if err != nil {
-		panic(err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+		return
 	}
-	fmt.Println(weatherResponse.Temperature)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
